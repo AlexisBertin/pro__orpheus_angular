694c92964c228f89dc1660d68cbc7a5b73c2d4ce
Alexis Bertin <alexis.bertin@propylon.com>
2015-06-17 14:44:15 +0100

    building environment

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orpheusHtmlTemplatesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the orpheusHtmlTemplatesApp
+ */
+angular.module('orpheusHtmlTemplatesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
