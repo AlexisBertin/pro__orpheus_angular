3c364d3119c0a65720d41b1cfa80ae1432e836e5
Alexis Bertin <alexis.bertin@propylon.com>
2015-06-19 17:40:49 +0100

    v0.01 - Header + Nav bar CSS

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,25 @@
  * Controller of the orpheusHtmlTemplatesApp
  */
 angular.module('orpheusHtmlTemplatesApp')
-  .controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+
+    // Dashboard navbar search input
+    var dashboardSearchIcon = $('.dashboard--search label i').css('color');
+    $('.dashboard---searchInput').focus(function(){
+    	$('.dashboard--search label i').css({'color':'#3F51B5'});
+    });
+    $('.dashboard---searchInput').blur(function(){
+    	$('.dashboard--search label i').css({'color':dashboardSearchIcon});
+    });
+
+
+
+
+
+});
